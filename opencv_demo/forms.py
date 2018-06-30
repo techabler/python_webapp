0cfcb59997d1ae818edd67a40b2a3d0774304397
DustinP <tabler@daum.net>
2018-07-01 01:07:36 +0900

    first commit

---
+++ opencv_demo/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class UploadImageForm(forms.Form):
+  title = forms.CharField(max_length=50)
+  #file = forms.FileField()
+  image = forms.ImageField()
+
+
+
+
+
---
+++ opencv_demo/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .forms import UploadImageForm
+from django.core.files.storage import FileSystemStorage
+
+# templates 폴더 아래에
+def first_view(request):
+    return render(request, 'opencv_demo/first_view.html', {})
+
+
+def uimage(request):
+    if request.method == 'POST':
+        form = UploadImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            myfile = request.FILES['image']
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            uploaded_file_url = fs.url(filename)
+            return render(request, 'opencv_demo/uimage.html', {'form': form, 'uploaded_file_url': uploaded_file_url})
+    else:
+        form = UploadImageForm()
+        return render(request, 'opencv_demo/uimage.html', {'form': form})
+
+
